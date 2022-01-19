aa19eed90fb3e9589947d23f9eacadec7ad0502d
uvych <goshauvych@gmail.com>
2022-01-19 23:08:05 +0300

    init commit

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -3,4 +3,11 @@ public interface Versions {
     String junit = "5.7.0";
     String assertJ = "3.21.0";
     String mockito = "4.0.0";
+    String jmh = "1.33";
+    String jol = "0.16";
+    String asm = "9.2";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.19.1";
+
+    String testcontainers = "1.15.3";
 }
